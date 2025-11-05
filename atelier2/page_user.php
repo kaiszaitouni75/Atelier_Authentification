83573ad981f187981e2c690e4a15aa47ae9c068f
kaiszaitouni75 <kaiszaitouni75@gmail.com>
2025-11-05 11:06:50 +0100

    Update page_user.php

--- atelier2/page_user.php
+++ atelier2/page_user.php
@@ -18,7 +18,7 @@ if (
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
+    <title>Accueil User</title>
 </head>
 <body>
     <h1>Bienvenue sur la page User protégée par un Cookie</h1>
