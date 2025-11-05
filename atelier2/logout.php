e737711cfc5537520c17407933bedfea450f202a
kaiszaitouni75 <kaiszaitouni75@gmail.com>
2025-11-05 10:42:18 +0100

    Update logout.php

--- atelier2/logout.php
+++ atelier2/logout.php
@@ -6,7 +6,7 @@ $_SESSION = [];
 session_destroy();
 
 // Supprimer le cookie d'authentification
-setcookie('authToken', '', time() - 3600, '/', '', false, true);
+setcookie('authToken', '', time() - 60, '/', '', false, true);
 
 // Redirection vers la page de connexion
 header('Location: index.php');
