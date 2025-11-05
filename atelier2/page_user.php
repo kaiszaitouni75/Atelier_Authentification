1637362ce41e0597451a142d155f72155db111fe
kaiszaitouni75 <kaiszaitouni75@gmail.com>
2025-11-05 10:51:04 +0100

    Update page_user.php

--- atelier2/page_user.php
+++ atelier2/page_user.php
@@ -1,27 +1,25 @@
 <?php
+// Démarrer la session
 session_start();
 
-// Vérifier cookie + token + rôle utilisateur
-if (
-    !isset($_COOKIE['authToken']) ||
-    !isset($_SESSION['authToken']) ||
-    $_COOKIE['authToken'] !== $_SESSION['authToken'] ||
-    !isset($_SESSION['role']) ||
-    $_SESSION['role'] !== 'user'
-) {
+// Vérifier si l'utilisateur est bien en possession d'un cookie valide
+// Dans le cas contraire il sera redirigé vers la page d'accueil de connexion
+if (!isset($_COOKIE['authToken']) || !isset($_SESSION['authToken']) || $_COOKIE['authToken'] !== $_SESSION['authToken']) {
     header('Location: index.php');
     exit();
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Espace Utilisateur</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
 </head>
 <body>
-    <h1>Bienvenue sur la page USER</h1>
-    <p>Vous êtes connecté en tant qu'utilisateur standard ✅</p>
+    <h1>Bienvenue sur la page Administrateur protégée par un Cookie</h1>
+    <p>Vous êtes connecté en tant qu'admin.</p>
     <a href="logout.php">Se déconnecter</a>
 </body>
 </html>
