71750e65758d0961eb899e90811789960090e905
kaiszaitouni75 <kaiszaitouni75@gmail.com>
2025-11-05 10:25:14 +0100

    Update index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -2,11 +2,11 @@
 setcookie('lang', 'fr', time() + 60, null, null, false, true);
 // Démarrer une session utilisateur qui sera en mesure de pouvoir gérer les Cookies
 session_start();
-$random_number = bin2hex(random_bytes(16)); 
+$_SESSION['random_number'] = bin2hex(random_bytes(16)); 
 // Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
 // Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
 // Dans le cas contraire il devra s'identifier.
-if (isset($_COOKIE['authToken']) && $_COOKIE['authToken'] === $random_number) {
+if (isset($_COOKIE['authToken']) && $_COOKIE['authToken'] === 'random_number') {
     header('Location: page_admin.php');
     exit();
 }
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Vérification simple du username et de son password.
     // Si ok alors on initialise le cookie sur le poste de l'utilisateur 
     if ($username === 'admin' && $password === 'secret') {
-        setcookie('authToken', $random_number, time() + 3600, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
+        setcookie('authToken', 'random_number', time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
         header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
     } else {
